fea37dab4d26eb0e9c09ef3a8e4519e91af97d23
siham heddi <siham.heddi.dev@gmail.com>
2022-11-24 12:42:26 +0100

    forms and v-model

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="hello">
-     <p>
-        skkskskskskskskskks
-     </p>
+      <input type="text" :value="salary" @input="salary=$event.target.value">
+       <input type="number" v-model.number="counter">
+      <br>
+      
+      <select v-model="levels" multiple>
+          
+          <option v-for="({name,value},index) in options" :key="index" :value="value" >
+            {{name}}
+          </option>
+          
+
+      </select>
+      
+
+      <br>
+      
+      <input type="checkbox" name="sex" value="man" id="man" v-model="sex">
+      <label for="man">man</label>
      
-     <!-- Event modifier -->
+      <br>
+     
+      <input type="checkbox" name="sex" value="woman" id="woman" v-model="sex">
+      <label for="woman">woman</label>
+      <br>
 
-     <button type="submit" @click.prevent="($event)=>submit($event)">
-        submit
-     </button>
+      <input type="checkbox" name="sex" value="other"  id="other" v-model="sex">
+      <label for="other">other</label>
+      <br>
+      
+<!--       <input type="radio" value="car" id="car" name="vehicule" :checked="car" @change="car = !$event.target.checked">
+      <label for="car">car</label>
+             
+      <input type="radio" value="bus" id="bus"  name="vehicule" :checked="bus"  @change="bus = $event.target.checked">
+      <label for="bus">bus</label> -->
 
-     {{ok2}}
-   
-    <input type="text" :value="count" @keyup.enter="count++">
   </div>
 </template>
 
@@ -20,16 +42,40 @@
 export default {
   name: 'HelloWorld',
   methods:{
-    submit(e){
-      console.log(e);
-      this.ok2 = true
-    }
+   
   },
   data(){
     return {
-      count:0,
-      ok1:true,
-      ok2:false,
+
+      salary:10000,
+      counter:4,
+      levels:[],
+      sex:[],
+      car:false,
+      bus:true,
+      options:[
+        {
+          name:"level1",
+          value:1
+        },
+        {
+          name:"level2",
+          value:2
+        },
+        {
+          name:"level3",
+          value:3
+        },
+        {
+          name:"level4",
+          value:4
+        },
+        {
+          name:"level5",
+          value:5
+        }
+      ]
+    
       
      
     }

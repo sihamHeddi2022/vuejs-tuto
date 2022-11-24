1994f2c92bf4c1c20600c9222f8029681f301b17
siham heddi <siham.heddi.dev@gmail.com>
2022-11-24 10:50:12 +0100

    computedproperty+binding

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,61 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+    {{msg}}
+    <div :id="id" v-html='content'></div>
+    <a :href="url">google</a>
+    {{sum()}}
+    {{ ok? "yeah I'm here" : "No . I don't want to meet her. I hate her "}}
+    <p v-if="ok">Go out right now and don't back again to my house</p>
+    <button @click="submit">submit</button>
+    <p>{{concatenate}}</p>
+    <span :[attr]="red">hi there</span>
+    <p :style="[style,style2]">dsjdsjds sdjdsjdsjdsjjds</p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  methods:{
+    sum(){
+      return 1+1
+    },
+
+    submit(){
+      this.ok = false
+    }
+  },
+  computed:{
+    concatenate(){
+      return this.attr+"  "+this.id
+    }
+  },
+  data(){
+    return {
+      id:"ksksk",
+      ok:true,
+      url:"www.google.com",
+      msg :"  hello world !",
+      content:"<b>hi there</b>",
+      attr :"class",
+      red:"red",
+      style:{
+        fontSize:"18px",
+        color:"green"
+      },
+      style2:{
+        textDecoration:"underline"
+      }
+    }
   }
+
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.red {
+  color:red
 }
 </style>

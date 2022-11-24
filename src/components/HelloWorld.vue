c0e241af36b8c61027a71e4217cdf6dfdb3ae773
siham heddi <siham.heddi.dev@gmail.com>
2022-11-24 11:36:28 +0100

    v-for+v-if

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,52 +1,50 @@
 <template>
   <div class="hello">
-
-    {{msg}}
-    <div :id="id" v-html='content'></div>
-    <a :href="url">google</a>
-    {{sum()}}
-    {{ ok? "yeah I'm here" : "No . I don't want to meet her. I hate her "}}
-    <p v-if="ok">Go out right now and don't back again to my house</p>
-    <button @click="submit">submit</button>
-    <p>{{concatenate}}</p>
-    <span :[attr]="red">hi there</span>
-    <p :style="[style,style2]">dsjdsjds sdjdsjdsjdsjjds</p>
+   
+     <p v-for="item in array1" :key="item.id">
+         {{item}}
+     </p>
+       <p v-for="item in arrayObject" :key="item.id">
+         {{item.name}}
+       </p>
+      <hr>
+        <p v-for="({name},index) in arrayObject" :key="index">
+         {{name}} -  {{index}} 
+       </p>
+          <hr>
+        <p v-for="n in 10" :key="n.id">
+          {{n}}
+        </p>
+        <h1 v-if="ok2">
+              kskkskskskksks
+        </h1>
+        <p v-else>nothing</p>
+        <!-- you can use v-if in template -->
+        <p v-for="n in 10" :key="n" >
+          <span  v-if="ok1">{{n}}</span>
+        </p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  methods:{
-    sum(){
-      return 1+1
-    },
-
-    submit(){
-      this.ok = false
-    }
-  },
-  computed:{
-    concatenate(){
-      return this.attr+"  "+this.id
-    }
-  },
+ 
   data(){
     return {
-      id:"ksksk",
-      ok:true,
-      url:"www.google.com",
-      msg :"  hello world !",
-      content:"<b>hi there</b>",
-      attr :"class",
-      red:"red",
-      style:{
-        fontSize:"18px",
-        color:"green"
-      },
-      style2:{
-        textDecoration:"underline"
-      }
+      ok1:true,
+      ok2:false,
+      array1:["hdhdhd","dhdhdh","jjdjdjdjd"],
+      arrayObject:[
+        {
+         name:"suzan",
+
+        },
+        {
+         name:"amel"
+        }
+      ]
+     
     }
   }
 
@@ -55,7 +53,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.red {
-  color:red
-}
+
 </style>
